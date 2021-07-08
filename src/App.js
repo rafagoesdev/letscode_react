d0adebc3c02db9cf49ed1d2ec02e7b3cfaefd8c1
Rafael Goes <rafagoes.dev@gmail.com>
2021-07-08 16:53:00 -0300

    Criando componente simples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React from "react";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
 
-    </div>
-  );
+    render(){
+        return (
+          <>
+            <p>Meu primeiro parágrafo em React.</p>
+            <p>Meu segundo parágrafo em React.</p>
+            <div>
+              <pre>Cansei de parágrafos...</pre>
+            </div>
+            <p>{ new Date().toLocaleDateString("pt-BR") }</p>
+          </>
+        );
+    }
 }
 
 export default App;
