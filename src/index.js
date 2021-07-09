c0e265515c19f77802bf2589b6870bffc4d7e7b6
Rafael Goes <rafagoes.dev@gmail.com>
2021-07-09 13:28:02 -0300

    Ciclo de vida

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import ReactDOM from 'react-dom';
 import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
+import App5 from './components/App5';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <App5></App5>
     <App4></App4>
     <App3></App3>
     <App2 title="Titulo">
