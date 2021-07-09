bb3dc9971ceff934895f993251e6d56480f7df79
Rafael Goes <rafagoes.dev@gmail.com>
2021-07-09 15:11:59 -0300

    Listas

--- src/components/App2.js
+++ src/components/App2.js
@@ -4,6 +4,7 @@ class App2 extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {};
     }
     render(){
         return (
--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
 import App5 from './components/App5';
+import Lista from './components/Lista';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Lista></Lista>
     <App5></App5>
     <App4></App4>
     <App3></App3>
