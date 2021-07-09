a39f716bff6913d148a2339501cabafeee48faba
Rafael Goes <rafagoes.dev@gmail.com>
2021-07-09 11:23:10 -0300

    Renderização condicional (App4)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './components/App';
+// import App from './components/App';
 import App2 from './components/App2';
 import App3 from './components/App3';
+import App4 from './components/App4';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <App4></App4>
     <App3></App3>
     <App2 title="Titulo">
       Conteúdo
